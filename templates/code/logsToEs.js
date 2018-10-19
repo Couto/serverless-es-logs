a630ab3bd1bd6c6d7a614cf6b55d350671714a01
Luis Couto <couto@yld.io>
2018-10-19 17:17:04 +0100

    Only send logs if they exist

--- templates/code/logsToEs.js
+++ templates/code/logsToEs.js
@@ -6,13 +6,14 @@ const transform = require('./transform');
 const gunZip = utils.promisify(zlib.gunzip);
 const toUTF8 = data => data.toString('utf8');
 const toJSON = data => JSON.parse(data);
+const ifTruthy = fn => data => data && fn(data);
 
 exports.handler = async (input, context) =>
   gunZip(new Buffer(input.awslogs.data, 'base64'))
     .then(toUTF8)
     .then(toJSON)
     .then(transform)
-    .then(request)
+    .then(ifTruthy(request))
     .then(success => {
       console.log('Response:', JSON.stringify(success));
       context.succeed('Success');
